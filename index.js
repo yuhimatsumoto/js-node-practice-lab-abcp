1c71f4996bd7a89becf31329d12a8f95d1711678
Yuhi Matsumoto <ym.starbuckslover@icloud.com>
2018-01-24 09:02:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // write your code below!
 
 var name = "Yuhi"
-var height = "74"
+var height = "157"
 
 console.log("Name:")
 console.log(name)
 
-console.log("Height:")
+console.log("Height+1")
 console.log(height)
